d81e45e4b5c242aebd1fd77712704658bfab013e
Erin Murphy <erin@Erins-MacBook-Pro.local>
2024-10-31 11:37:36 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Splonk 2.0`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
